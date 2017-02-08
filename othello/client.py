31cb96049038df6b7a1211137bf571f5e839cb27
Jason Liu <jliu1052@gmail.com>
2017-02-08 12:49:30 -0500

    Fixed othello with alpha-beta pruning and minimax

--- othello/client.py
+++ othello/client.py
@@ -3,7 +3,6 @@ import pickle
 import random
 from time import time
 from jliu_strategy import Strategy      # first file
-from rehan_strategy import Strategy99  # second file
 import Othello_Core as core
 
 tic = time()
@@ -14,7 +13,7 @@ s = Strategy()   # name of class in first strategy
 v = Strategy()  # name of class in second strategy
 
 s1 = s.alphabeta_strategy(3)    # actually calling of method in class; returns a strategy method
-s2 = v.alphabeta_strategy(3)    # ditto
+s2 = v.human_strategy(3)    # ditto
 
 def play(strategy_X, strategy_O, first, silent=True):
     """
