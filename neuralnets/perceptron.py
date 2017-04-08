7bdda89b0a541b9315ef7bff57de3a3821d403f6
Jason Liu <jliu1052@gmail.com>
2017-04-08 19:39:13 -0400

    Updated perceptron.py

--- neuralnets/perceptron.py
+++ neuralnets/perceptron.py
@@ -1,29 +1,53 @@
 import numpy as np
+import itertools
 
 # learning rate
 lamb = 1
-epoch = 15
+epoch = 500
+n = 4
 
 def find_weight(training_set):
-    w = np.array([0.5, 0.5, 0.5])
+    w = np.array([0.5]*(n+1))
     for val in range(epoch):
-        for x, fx in training_set:
+        for (x, fx) in training_set:
             f = actuator(np.dot(x, w))
             w = w + (lamb*(fx - f))*x
-    accuracy = 1 - (sum([abs(fx-actuator(np.dot(x, w))) for x, fx in training_set]))/len(training_set)
-    return w, accuracy
+    accuracy = 1 - (sum([abs(fx-actuator(np.dot(x, w))) for (x, fx) in training_set]))/len(training_set)
+    return (w, accuracy)
 
 def actuator(val):
     return 1 if val > 0 else 0
 
-def andOperator(arr):
-    return 1 if val == 0 for val in arr
-
 # takes n (binary number)
 def create_set(val):
     ts = []
-    retur ts
+    binaries = ["".join(seq) for seq in itertools.product("01", repeat=val)]
+    for index in range(2**(2**val)):
+        row = []
+        for binary in binaries:
+            tempBin = [int(d) for d in binary]
+            tempBin.append(1)
+            tempBin = np.array(tempBin)
+            bi = bin(index)[2:].zfill(2**n)
+            row.append((tempBin, int(bi[binaries.index(binary)])))
+        ts.append(row)
+    return ts
 
-training_set = [(np.array([0, 0, 1]), 0), (np.array([1, 0, 1]), 0), (np.array([0, 1, 1]), 0), (np.array([1, 1, 1]), 1)]
-weight = find_weight(training_set)
-print(weight)
+arrTS = create_set(n)
+"""
+for ts in arrTS:
+    print(ts)
+"""
+output = []
+total = 0
+correct = 0
+for ts in arrTS:
+    vector, accuracy = find_weight(ts)
+    if accuracy == 1.0:
+        correct += 1
+accuracy = correct/len(arrTS)
+print(correct)
+print(accuracy)
+# training_set = [(np.array([0, 0, 1]), 0), (np.array([1, 0, 1]), 0), (np.array([0, 1, 1]), 0), (np.array([1, 1, 1]), 1)]
+# weight = find_weight(training_set)
+# print(weight)
